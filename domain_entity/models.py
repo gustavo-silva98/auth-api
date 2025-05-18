dd7296820c3bdbfd513440c6410205c19f2a72cc
gustavo-silva98 <silva.gustavo98@gmail.com>
2025-05-17 22:14:03 -0300

    feat: Criada classe de model para User

--- domain_entity/models.py
+++ domain_entity/models.py
@@ -1,1 +1,20 @@
-pass
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(20))
+    email: Mapped[str]
+    fullname: Mapped[str]
+    password: Mapped[str]
+
+    def __repr__(self) -> str:
+        return f'User (id={self.id}, username={self.username},'
+        f'email={self.email}, fullname={self.fullname})'
