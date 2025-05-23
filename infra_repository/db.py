bc0886bfcc03777630259d7b31baebcc4ef8377f
gustavo-silva98 <silva.gustavo98@gmail.com>
2025-05-22 23:23:12 -0300

    Arrumando tipos das classes

--- infra_repository/db.py
+++ infra_repository/db.py
@@ -12,27 +12,14 @@ from settings import Settings
 
 # Interface de handler de banco de dados.
 class DatabaseHandler:
-    def __init__(self):
-        self.engine: AsyncEngine | None = None
-        self.session_factory: async_sessionmaker[AsyncSession] | None = None
-
-    def create_engine(self, settings: Settings) -> None:
-        self.engine = create_async_engine(settings.DB_URL, echo=True)
-
-    def create_session_factory(self) -> None:
-        self.session_factory = async_sessionmaker(
-            bind=self.engine, expire_on_commit=False
+    def __init__(self, settings: Settings):
+        self.engine: AsyncEngine = create_async_engine(
+            settings.DB_URL, echo=True
         )
+        self.session_factory: async_sessionmaker[
+            AsyncSession
+        ] = async_sessionmaker(bind=self.engine, expire_on_commit=False)
 
     async def get_db(self) -> AsyncGenerator[AsyncSession, None]:
-        if self.session_factory is None:
-            self.create_session_factory()
-
         async with self.session_factory() as session:
             yield session
-
-
-settings = Settings()   # Objeto que retorna os valores de .env
-
-if settings.DB_URL is None:
-    raise RuntimeError('A variável DATABASE_URL não foi definida')
--- settings.py
+++ settings.py
@@ -1,9 +1,10 @@
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv()
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings:
-    DB_URL : str = os.getenv('DB_URL')
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        case_sensitive=True,
+    )
+    DB_URL: str  # Não use Field(...) se não for obrigatório
