794bcc8b9b44204fdd2222d17f5c21f59cdc687f
gustavosilva <silva.gustavo98@gmail.com>
2025-06-03 01:40:06 -0300

    refact: Refatoração com Injecao de independencia e Inversao de dependencia na classe AuthService.

--- application_service/auth_service.py
+++ application_service/auth_service.py
@@ -1,3 +1,5 @@
+from typing import Protocol
+
 from passlib.context import CryptContext
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -7,38 +9,48 @@ from domain_entity.schemas import UserCreateDTO
 from infra_repository.crud import UserCRUD
 
 
-class AuthService:
-    @staticmethod
-    async def create_user_from_route(
-        user: UserCreateDTO, user_crud: UserCRUD, db: AsyncSession
-    ):
-        get_user = await user_crud.get_user_by_email(
-            email=user.email, async_transaction=db
-        )
-        if get_user:
-            raise DuplicateUserError('Usuário já cadastrado.')
+class HasherProtocol(Protocol):
+    def hash(self,password : str) -> str:
+        ...
 
+class BcryptHasher(HasherProtocol):
+    def __init__(self,context: CryptContext):
+        self.context = context
+
+    def hash(self,password: str) -> str:
+        return self.context.hash(password)
+    
+
+
+class AuthService:
+    def __init__(self,hasher: HasherProtocol,user_crud: UserCRUD,db: AsyncSession):
+        self.hasher = hasher
+        self.user_crud = user_crud
+        self.db = db
+    
+    async def create_user_from_route(self,user : UserCreateDTO):
+
+        get_user = await self.user_crud.get_user_by_email(
+            email= user.email,async_transaction=self.db
+        )
+
+        if get_user:
+            raise DuplicateUserError("Usuário já cadastrado.")
+        
         if user.pwd_plain != user.confirm_pwd_plain:
-            print(user.confirm_pwd_plain)
             raise PasswordNotMatch()
 
-        pwd_hash = Hasher.hash_password(user.pwd_plain)
+        pwd_hash = self.hasher.hash(user.pwd_plain)
 
-        result = await user_crud.insert_user(
+        result = await self.user_crud.insert_user(
             User(
                 username=user.username,
                 email=user.email,
                 fullname=user.fullname,
                 password=pwd_hash,
             ),
-            db,
+            self.db
         )
+
         return result
 
-
-class Hasher:
-    __pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-
-    @staticmethod
-    def hash_password(password):
-        return Hasher.__pwd_context.hash(password)
--- tests/testa_auth_user.py
+++ tests/testa_auth_user.py
@@ -1,12 +1,19 @@
 from unittest.mock import AsyncMock
 
 import pytest
+from passlib.context import CryptContext
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from application_service.auth_service import AuthService
+from application_service.auth_service import AuthService, BcryptHasher
 from domain_entity.exceptions import DuplicateUserError, PasswordNotMatch
 from domain_entity.schemas import UserCreateDTO, UserFromDBDTO
 from infra_repository.crud import UserCRUD
+
+"""
+Métodos AuthService
+- create_user_from_route - OK
+- login_user_from_route - //TODO
+"""
 
 
 @pytest.fixture
@@ -19,6 +26,9 @@ def user_create_dto():
         confirm_pwd_plain='pwd plain Teste',
     )
 
+@pytest.fixture
+def get_hasher():
+    return BcryptHasher(context=CryptContext(schemes=['bcrypt'],deprecated='auto'))
 
 @pytest.fixture
 def mock_user_from_db():
@@ -30,52 +40,60 @@ def mock_user_from_db():
     )
 
 
-async def testa_service_create_user_valido(user_create_dto, mock_user_from_db):
+async def testa_service_create_user_valido(
+    user_create_dto, mock_user_from_db, get_hasher
+):
+
     mock_db = AsyncMock(spec=AsyncSession)
 
-    usuario_criar = user_create_dto
+    user_create = user_create_dto
 
     mock_return = mock_user_from_db
 
     user_crud = UserCRUD()
+    auth_service = AuthService(get_hasher,user_crud=user_crud,db=mock_db)
+    
     user_crud.insert_user = AsyncMock(return_value=mock_return)
     user_crud.get_user_by_email = AsyncMock(return_value=None)
 
-    result = await AuthService.create_user_from_route(
-        usuario_criar, user_crud=user_crud, db=mock_db
-    )
+    result = await auth_service.create_user_from_route(user=user_create)
 
     assert result == mock_return
     user_crud.insert_user.assert_called_once()
 
 
-async def testa_service_create_user_duplicated(user_create_dto):
+async def testa_service_create_user_duplicated(user_create_dto,get_hasher):
     mock_db = AsyncMock(spec=AsyncSession)
-
-    user_create = user_create_dto
 
     user_crud = UserCRUD()
 
     return_mock = mock_user_from_db
 
     user_crud.get_user_by_email = AsyncMock(retur_value=return_mock)
+    auth_service = AuthService(get_hasher,user_crud,mock_db)
 
     with pytest.raises(DuplicateUserError):
-        await AuthService.create_user_from_route(
-            user=user_create, user_crud=user_crud, db=mock_db
-        )
+        await auth_service.create_user_from_route(user=user_create_dto)
 
 
-async def testa_create_user_pwd_dont_match(user_create_dto):
+async def testa_create_user_pwd_dont_match(user_create_dto,get_hasher):
     mock_db = AsyncMock(spec=AsyncSession)
 
     user_create = user_create_dto
     user_create.pwd_plain = 'senha não bate'
     user_crud = UserCRUD()
     user_crud.get_user_by_email = AsyncMock(return_value=None)
+    auth_service = AuthService(get_hasher,user_crud,mock_db)
 
     with pytest.raises(PasswordNotMatch):
 
-        await AuthService.create_user_from_route(
-            user=user_create, user_crud=user_crud, db=mock_db
-        )
+        await auth_service.create_user_from_route(user=user_create_dto)
+
+
+"""
+
+async def authenticate_and_get_token_pwd_unmatch(auth_request_dto):
+    mock_db = AsyncMock(spec=AsyncSession)
+
+    Hasher = AuthService.Hasher
+"""
