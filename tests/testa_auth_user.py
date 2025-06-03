92a6e23d0a406c7c8f6a6c1610532c8583edc8a4
gustavo-silva98 <silva.gustavo98@gmail.com>
2025-06-03 02:08:11 -0300

    teste pre-commit

--- tests/testa_auth_user.py
+++ tests/testa_auth_user.py
@@ -26,9 +26,13 @@ def user_create_dto():
         confirm_pwd_plain='pwd plain Teste',
     )
 
+
 @pytest.fixture
 def get_hasher():
-    return BcryptHasher(context=CryptContext(schemes=['bcrypt'],deprecated='auto'))
+    return BcryptHasher(
+        context=CryptContext(schemes=['bcrypt'], deprecated='auto')
+    )
+
 
 @pytest.fixture
 def mock_user_from_db():
@@ -51,8 +55,8 @@ async def testa_service_create_user_valido(
     mock_return = mock_user_from_db
 
     user_crud = UserCRUD()
-    auth_service = AuthService(get_hasher,user_crud=user_crud,db=mock_db)
-    
+    auth_service = AuthService(get_hasher, user_crud=user_crud, db=mock_db)
+
     user_crud.insert_user = AsyncMock(return_value=mock_return)
     user_crud.get_user_by_email = AsyncMock(return_value=None)
 
@@ -62,7 +66,7 @@ async def testa_service_create_user_valido(
     user_crud.insert_user.assert_called_once()
 
 
-async def testa_service_create_user_duplicated(user_create_dto,get_hasher):
+async def testa_service_create_user_duplicated(user_create_dto, get_hasher):
     mock_db = AsyncMock(spec=AsyncSession)
 
     user_crud = UserCRUD()
@@ -70,20 +74,20 @@ async def testa_service_create_user_duplicated(user_create_dto,get_hasher):
     return_mock = mock_user_from_db
 
     user_crud.get_user_by_email = AsyncMock(retur_value=return_mock)
-    auth_service = AuthService(get_hasher,user_crud,mock_db)
+    auth_service = AuthService(get_hasher, user_crud, mock_db)
 
     with pytest.raises(DuplicateUserError):
         await auth_service.create_user_from_route(user=user_create_dto)
 
 
-async def testa_create_user_pwd_dont_match(user_create_dto,get_hasher):
+async def testa_create_user_pwd_dont_match(user_create_dto, get_hasher):
     mock_db = AsyncMock(spec=AsyncSession)
 
     user_create = user_create_dto
     user_create.pwd_plain = 'senha não bate'
     user_crud = UserCRUD()
     user_crud.get_user_by_email = AsyncMock(return_value=None)
-    auth_service = AuthService(get_hasher,user_crud,mock_db)
+    auth_service = AuthService(get_hasher, user_crud, mock_db)
 
     with pytest.raises(PasswordNotMatch):
 
